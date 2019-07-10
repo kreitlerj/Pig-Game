354166095269d74d137ccbc1e58a55de6280797f
kreitlerj <kreitlerj11@yahoo.com>
2019-07-10 10:55:35 -0500

    Implementing the hold button functionality

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
 
-// Setting up the event listener for the roll dice button
+// Event listener for the roll dice button
 document.querySelector('.btn-roll').addEventListener('click', function() {
     // Random number
     var dice = Math.floor(Math.random() * 6) + 1;
@@ -35,6 +35,50 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.src = 'dice-' + dice + '.png';
 
     // Update the round score IF the rolled number was NOT a 1
-
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+    } else {
+        // Next player
+        nextPlayer();
+    }
 
 });
+
+// Event listener for the hold button
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add CURRENT score to player's GLOBAL score
+    scores[activePlayer] += roundScore;
+
+    // Update UI
+    document.querySelector('#score-'+ activePlayer).textContent = scores[activePlayer];
+
+    // Check if player won the game
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        // Next player
+        nextPlayer();
+    }
+});
+
+function nextPlayer() {
+    // Switch the active player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    
+    // Reset round score and the UI
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // Toggle active class
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // Hide the dice
+    document.querySelector('.dice').style.display = 'none';
+};
